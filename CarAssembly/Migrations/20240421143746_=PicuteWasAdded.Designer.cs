ab483a0d87edfb845d32f7847fac6c05621d7111
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-04-21 18:39:29 +0300

    migrations added

--- CarAssembly/Migrations/20240421143746_=PicuteWasAdded.Designer.cs
+++ CarAssembly/Migrations/20240421143746_=PicuteWasAdded.Designer.cs
@@ -4,6 +4,7 @@ using CarAssembly.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarAssembly.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240421143746_=PicuteWasAdded")]
+    partial class PicuteWasAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarAssembly/Migrations/20240421143746_=PicuteWasAdded.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarAssembly.Migrations
+{
+    /// <inheritdoc />
+    public partial class PicuteWasAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Photo",
+                table: "Assemblies",
+                newName: "Picture");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Year",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Kilometers",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Assemblies",
+                newName: "Photo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Kilometers",
+                table: "Cars",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
