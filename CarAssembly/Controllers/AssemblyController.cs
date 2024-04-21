0ca6eba493b6cdd2020a1365c702b61588290f95
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-04-21 18:37:30 +0300

    Assembly was added and functionality is woking properly

--- CarAssembly/Controllers/AssemblyController.cs
+++ CarAssembly/Controllers/AssemblyController.cs
@@ -1,5 +1,9 @@
-﻿using CarAssembly.Models;
+﻿using AutoMapper;
+using CarAssembly.Database.Models;
+using CarAssembly.ModelDtos.Assembly;
+using CarAssembly.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarAssembly.Controllers
 {
@@ -7,29 +11,84 @@ namespace CarAssembly.Controllers
     {
         //dependacy injetion
         private readonly ApplicationDbContext data;
-        public AssemblyController(ApplicationDbContext data)
+        private readonly IMapper mapper;
+        public AssemblyController(ApplicationDbContext data, IMapper mapper)
         {
             this.data = data;
+            this.mapper = mapper;
         }
         [HttpGet]
-        public IActionResult Add()
+        public IActionResult AddAssembly()
         {
-            return View();
+
+            AssemblyFormDto assembly = new AssemblyFormDto();
+            assembly.Cars = this.data.Cars.ToList();
+            assembly.Categories = this.data.Categories.ToList();
+            foreach (var cond in Enum.GetValues(typeof(Condtion)))
+            {
+                assembly.Condtions.Add(cond.ToString());
+            }
+
+            return View(assembly);
         }
         [HttpPost]
-        public IActionResult Add(Assembly assembly)
+        public IActionResult AddAssembly(AssemblyFormDto assembly, List<IFormFile> Picture)
         {
+
             if (String.IsNullOrEmpty(assembly.Name))
             {
                 // message: try again 
                 TempData["error"] = "Try Again";
+                assembly.Categories = this.data.Categories.ToList();
+
+                foreach (var cond in Enum.GetValues(typeof(Condtion)))
+                {
+                    assembly.Condtions.Add(cond.ToString());
+                }
+
                 return View();
             }
 
-            this.data.Assemblies.Add(assembly);
+            var empDTO1 = mapper.Map<Assembly>(assembly);
+
+            byte[] photo = new byte[8000];
+            foreach (var item in Picture)
+            {
+                if (item.Length > 0)
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        item.CopyToAsync(stream);
+                        photo = stream.ToArray();
+                    }
+                }
+            }
+
+            empDTO1.Picture = photo;
+
+            this.data.Assemblies.Add(empDTO1);
             this.data.SaveChanges();
 
-            return View();
+            return RedirectToAction("Add", "Assembly");
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> SavePhoto(IFormFile Picture, AssemblyFormDto fromDto)
+        //{
+        //    if (Picture != null && Picture.Length > 0)
+        //    {
+        //        byte[] photoBytes;
+        //        using (var memoryStream = new MemoryStream())
+        //        {
+        //            await Picture.CopyToAsync(memoryStream);
+        //            photoBytes = memoryStream.ToArray();
+
+        //        }
+        //    }
+
+        //    return RedirectToAction("Add Picture"); // Redirect to home page after photo is saved
+        //}
+
     }
 }
+
