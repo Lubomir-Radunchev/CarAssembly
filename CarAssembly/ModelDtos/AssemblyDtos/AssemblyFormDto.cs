dd99453ac46fff977456b866cf1679c1743b2ca0
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-04-21 18:42:27 +0300

    car and assembly dto was added

---
+++ CarAssembly/ModelDtos/AssemblyDtos/AssemblyFormDto.cs
@@ -0,0 +1,28 @@
+﻿using CarAssembly.Database.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarAssembly.ModelDtos.Assembly
+{
+    public class AssemblyFormDto
+    {
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        public string Condtion { get; set; }
+
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        public byte[]? Picture { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+
+        public List<Category> Categories = new List<Category>();
+        public List<string> Condtions = new List<string>();
+        public List<Car> Cars = new List<Car>();
+    }
+}
---
+++ CarAssembly/ModelDtos/CarDtos/CarFromDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarAssembly.ModelDtos.CarDtos
+{
+    public class CarFromDto
+    {
+        public int Id { get; set; }
+       
+        [StringLength(50)]
+        public string Brand { get; set; }
+      
+        [StringLength(100)]
+        public string Model { get; set; }
+        [StringLength(100)]
+        public string Year { get; set; }
+        [StringLength(100)]
+        public string Kilometers { get; set; }
+
+    }
+}
