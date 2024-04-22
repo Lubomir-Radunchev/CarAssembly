86afb5715bcdc4ed79821a15f718b26e58e371f1
Lubomir-Radunchev <151566007+Lubomir-Radunchev@users.noreply.github.com>
2024-04-22 17:49:19 +0300

    ShowCar + ShowCarController

---
+++ CarAssembly/Controllers/ShowCarController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CarAssembly.Database.Models;
+using CarAssembly.ModelDtos.CarDtos;
+using CarAssembly.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarAssembly.Controllers
+{
+    public class ShowCarController : Controller
+    {
+        private readonly ApplicationDbContext data;
+        private readonly IMapper mapper;
+
+
+
+        public ShowCarController(ApplicationDbContext data, IMapper mapper)
+        {
+            this.mapper = mapper;
+            this.data = data;
+        }
+        [HttpGet]
+        public IActionResult ShowResults(ShowCar cars = null)
+        {
+
+            //if (cars != null && cars.CarForShowCar != null)
+            //{
+            //    //cars.CarForShowCar.Model.ToList();
+            //    //cars.CarForShowCar.Kilometers.ToList();
+            //    //cars.CarForShowCar.Year.ToList();
+            //    //cars.CarForShowCar.Brand.ToList();
+            //}
+            //else if (cars != null &&cars.AssemblyForShowCar != null)
+            //{
+            //    //cars.AssemblyForShowCar.Picture.ToArray();
+            //    //cars.AssemblyForShowCar.Condtion.ToList();
+            //    //cars.AssemblyForShowCar.Description.ToList();
+            //    //cars.AssemblyForShowCar.Name.ToList();
+            //}
+
+            this.data.ShowCars.Add(cars);
+            return View("ShowCarResult", cars);
+        }
+
+
+    }
+}
---
+++ CarAssembly/Database/Models/ShowCar.cs
@@ -0,0 +1,49 @@
+﻿using CarAssembly.ModelDtos.Assembly;
+using CarAssembly.ModelDtos.CarDtos;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace CarAssembly.Database.Models
+{
+    public class ShowCar
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null;
+        public readonly AssemblyFormDto AssemblyForShowCar;
+        public readonly CarFromDto CarForShowCar;
+        public ShowCar(AssemblyFormDto assembly, CarFromDto car)
+        {
+            this.AssemblyForShowCar = assembly;
+            this.CarForShowCar = car;
+
+
+        }
+        
+
+        public void ShowCarFromAssembly(AssemblyFormDto assembly)
+        {
+            this.AssemblyForShowCar.Condtion = assembly.Condtion;
+            this.AssemblyForShowCar.Description = assembly.Description;
+            this.AssemblyForShowCar.Category = assembly.Category;
+            this.AssemblyForShowCar.Picture = assembly.Picture;
+            
+        }
+        public void ShowCarFromCar(CarFromDto car)
+        {
+            this.CarForShowCar.Brand = car.Brand;   
+            this.CarForShowCar.Model = car.Model;
+            this.CarForShowCar.Year = car.Year;
+            this.CarForShowCar.Kilometers = car.Kilometers;
+           
+        }
+        public ShowCar()
+        {
+            CarForShowCar = new CarFromDto();
+            AssemblyForShowCar = new AssemblyFormDto();
+
+        }
+
+
+    }
+}
